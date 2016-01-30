bdd34f6b674da5cd7c8554ee65c10272bfe7c9c9
nikita-jajodia <nikita.jajodia@raweng.com>
2016-01-31 00:50:48 +0530

    noop

--- client/js/index.js
+++ client/js/index.js
@@ -16,10 +16,10 @@ $ = jQuery
 import App from './app';
 import About from './about';
 import Dashboard from './dashboard';
-import '../css/style.css';
+//import '../css/style.css';
 
 /*Require Bootstrap*/
-import '../css/libs/bootstrap.min.css';
+//import '../css/libs/bootstrap.min.css';
 import './libs/bootstrap.min.js'
 
 render((
